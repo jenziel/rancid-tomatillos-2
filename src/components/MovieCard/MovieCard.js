af047e63ebef5f32b0cc385729dec633bcb22913
Prissilla Escobar <prissilla988@gmail.com>
2023-10-15 11:02:18 -0600

    Pass in showYoutubeVideo and selectedTrailerKey to render in MovieCard :

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,8 +1,11 @@
 import './MovieCard.css'
 
-function MovieCard({ poster_path, title, showMovieDetails, id }) {
+function MovieCard({ poster_path, title, showMovieDetails, id, showYoutubeVideo }) {
     return (
-        <div className='card' onClick={() => showMovieDetails(id)}>
+        <div className='card' onClick={() => {
+            showMovieDetails(id)
+            showYoutubeVideo(id)
+            }}>
             <img src={poster_path} className='movie-poster' alt={`Movie poster for ${title}`} />
         </div>
     )
