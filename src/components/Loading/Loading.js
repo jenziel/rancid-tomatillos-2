a801ac6c3fd3d386d5611999405b95e3c5d6df2d
Prissilla Escobar <prissilla988@gmail.com>
2023-10-21 15:38:14 -0600

    Create Loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,12 @@
+import './Loading.css'
+
+function Loading() {
+
+    return (
+        <div className="loading">
+            <h1 className='loading-text'>Loading...</h1>
+        </div>
+    )
+}
+
+export default Loading
